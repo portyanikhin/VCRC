01e5431bf9b4f203f4993afbca451e3d172d7e8b
Vladimir Portyanikhin <portyanikhin@users.noreply.github.com>
2021-10-23 20:07:38 +0300

    Added unit tests for the TwoPhase enum

---
+++ VCRC.Tests/Enums/TestTwoPhase.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+using VCRC.Extensions;
+
+namespace VCRC.Tests
+{
+    public static class TestTwoPhase
+    {
+        [TestCase(TwoPhase.Bubble, ExpectedResult = 0)]
+        [TestCase(TwoPhase.Dew, ExpectedResult = 100)]
+        [TestCase(TwoPhase.Middle, ExpectedResult = 50)]
+        public static double TestVaporQuality(TwoPhase member) => member.VaporQuality().Percent;
+    }
+}
