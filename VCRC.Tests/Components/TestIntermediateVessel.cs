21c8bb68d6d32d1c60b9f01ff8c38b5b47d6c4d8
Vladimir Portyanikhin <portyanikhin@users.noreply.github.com>
2021-11-13 17:43:11 +0300

    Added the IntermediateVessel class and its unit tests

---
+++ VCRC.Tests/Components/TestIntermediateVessel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using FluentAssertions;
+using NUnit.Framework;
+using SharpProp;
+using UnitsNet;
+using UnitsNet.NumberExtensions.NumberToPressure;
+using UnitsNet.NumberExtensions.NumberToTemperature;
+using VCRC.Components;
+
+namespace VCRC.Tests.Components
+{
+    public static class TestIntermediateVessel
+    {
+        private static readonly Evaporator Evaporator =
+            new(FluidsList.R32, 5.DegreesCelsius(), TemperatureDelta.FromKelvins(8));
+
+        private static readonly Condenser Condenser =
+            new(FluidsList.R32, 50.DegreesCelsius(), TemperatureDelta.FromKelvins(3));
+
+        [Test]
+        public static void TestIntermediatePressure()
+        {
+            var intermediateVessel = new IntermediateVessel(Evaporator, Condenser);
+            intermediateVessel.Pressure.Pascals.Should()
+                .Be(Math.Sqrt(Evaporator.Pressure.Pascals * Condenser.Pressure.Pascals));
+        }
+
+        [Test]
+        public static void TestEquals()
+        {
+            var intermediateVessel = new IntermediateVessel(Evaporator, Condenser);
+            var intermediateVesselWithSamePressure = new IntermediateVessel(intermediateVessel.Pressure);
+            var intermediateVesselWithOtherPressure =
+                new IntermediateVessel(intermediateVessel.Pressure + 1.Atmospheres());
+            intermediateVessel.Should().Be(intermediateVesselWithSamePressure);
+            intermediateVessel.Should().BeSameAs(intermediateVessel);
+            intermediateVessel.Should().NotBeSameAs(intermediateVesselWithSamePressure);
+            intermediateVessel.Should().NotBe(intermediateVesselWithOtherPressure);
+            intermediateVessel.Should().NotBeNull();
+            intermediateVessel.Equals(new object()).Should().BeFalse();
+            (intermediateVessel == intermediateVesselWithSamePressure).Should()
+                .Be(intermediateVessel.Equals(intermediateVesselWithSamePressure));
+            (intermediateVessel != intermediateVesselWithOtherPressure).Should()
+                .Be(!intermediateVessel.Equals(intermediateVesselWithOtherPressure));
+        }
+    }
+}
