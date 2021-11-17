05a069476ff350fc5d9823bf4315a1567b91c8bc
Vladimir Portyanikhin <portyanikhin@users.noreply.github.com>
2021-11-17 17:21:01 +0300

    Added the VCRCWithEconomizerTPI class, its validator and unit tests

---
+++ VCRC.Tests/TestVCRCWithEconomizerTPI.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using FluentAssertions;
+using FluentValidation;
+using NUnit.Framework;
+using SharpProp;
+using UnitsNet;
+using UnitsNet.NumberExtensions.NumberToRatio;
+using UnitsNet.NumberExtensions.NumberToSpecificEnergy;
+using UnitsNet.NumberExtensions.NumberToTemperature;
+using VCRC.Components;
+using VCRC.Extensions;
+using VCRC.Fluids;
+
+namespace VCRC.Tests
+{
+    public class TestVCRCWithEconomizerTPI
+    {
+        private VCRCWithEconomizerTPI Cycle { get; set; } = null!;
+        
+        [SetUp]
+        public void SetUp()
+        {
+            const FluidsList refrigerantName = FluidsList.R32;
+            var evaporator = new Evaporator(refrigerantName, 5.DegreesCelsius(), TemperatureDelta.FromKelvins(8));
+            var compressor = new Compressor(80.Percent());
+            var condenser = new Condenser(refrigerantName, 50.DegreesCelsius(), TemperatureDelta.FromKelvins(3));
+            var economizer = new EconomizerTPI(evaporator, condenser, TemperatureDelta.FromKelvins(5));
+            Cycle = new VCRCWithEconomizerTPI(evaporator, compressor, condenser, economizer);
+        }
+        
+        [TestCase(Bound.Lower, "Intermediate pressure should be > evaporating pressure!")]
+        [TestCase(Bound.Higher, "Intermediate pressure should be < condensing pressure!")]
+        public void TestWrongIntermediatePressure(Bound bound, string message)
+        {
+            Action action = () =>
+                _ = new VCRCWithEconomizerTPI(Cycle.Evaporator, Cycle.Compressor, Cycle.Condenser,
+                    new EconomizerTPI(bound is Bound.Lower
+                            ? Cycle.Evaporator.Pressure
+                            : Cycle.Condenser.Pressure, 
+                        Cycle.Economizer.TemperatureDifference));
+            action.Should().Throw<ValidationException>().WithMessage($"*{message}*");
+        }
+
+        [Test]
+        public void TestWrongEconomizerTemperatureDifference()
+        {
+            Action action = () =>
+                _ = new VCRCWithEconomizerTPI(Cycle.Evaporator, Cycle.Compressor, Cycle.Condenser,
+                    new EconomizerTPI(Cycle.Economizer.Pressure, TemperatureDelta.FromKelvins(50)));
+            action.Should().Throw<ValidationException>()
+                .WithMessage("*Too high temperature difference at economizer 'cold' side!*");
+        }
+        
+        [Test]
+        public void TestSpecificMassFlows()
+        {
+            Cycle.FirstStageSpecificMassFlow.Should().Be(100.Percent());
+            Cycle.SecondStageSpecificMassFlow.Should()
+                .Be(Cycle.FirstStageSpecificMassFlow * (1 + (Cycle.Point2.Enthalpy - Cycle.Point3.Enthalpy) /
+                    (Cycle.Point3.Enthalpy - Cycle.Point9.Enthalpy)));
+        }
+
+        [Test]
+        [SuppressMessage("ReSharper", "InconsistentNaming")]
+        public void TestPoint2s()
+        {
+            Cycle.Point2s.Pressure.Should().Be(Cycle.Economizer.Pressure);
+            Cycle.Point2s.Entropy.Should().Be(Cycle.Point1.Entropy);
+            Cycle.Point2s.Phase.Should().Be(Phases.Gas);
+        }
+
+        [Test]
+        public void TestPoint2()
+        {
+            Cycle.Point2.Pressure.Should().Be(Cycle.Economizer.Pressure);
+            Cycle.Point2.Enthalpy.Should().Be(Cycle.Point1.Enthalpy + (Cycle.Point2s.Enthalpy - Cycle.Point1.Enthalpy) /
+                Cycle.Compressor.IsentropicEfficiency.DecimalFractions);
+            Cycle.Point2.Phase.Should().Be(Phases.Gas);
+        }
+
+        [Test]
+        public void TestPoint3()
+        {
+            Cycle.Point3.Pressure.Should().Be(Cycle.Economizer.Pressure);
+            Cycle.Point3.Quality.Should().Be(TwoPhase.Dew.VaporQuality());
+            Cycle.Point3.Phase.Should().Be(Phases.TwoPhase);
+        }
+
+        [Test]
+        [SuppressMessage("ReSharper", "InconsistentNaming")]
+        public void TestPoint4s()
+        {
+            Cycle.Point4s.Pressure.Should().Be(Cycle.Condenser.Pressure);
+            Cycle.Point4s.Entropy.Should().Be(Cycle.Point3.Entropy);
+            Cycle.Point4s.Phase.Should().Be(Phases.Gas);
+        }
+
+        [Test]
+        public void TestPoint4()
+        {
+            Cycle.Point4.Pressure.Should().Be(Cycle.Condenser.Pressure);
+            Cycle.Point4.Enthalpy.Should().Be(Cycle.Point3.Enthalpy + (Cycle.Point4s.Enthalpy - Cycle.Point3.Enthalpy) /
+                Cycle.Compressor.IsentropicEfficiency.DecimalFractions);
+            Cycle.Point4.Enthalpy.Should().BeGreaterThan(Cycle.Point4s.Enthalpy);
+            Cycle.Point4.Phase.Should().Be(Phases.Gas);
+        }
+
+        [Test]
+        public void TestPoint5()
+        {
+            Cycle.Point5.Pressure.Should().Be(Cycle.Condenser.Pressure);
+            Cycle.Point5.Quality.Should().Be(TwoPhase.Dew.VaporQuality());
+            Cycle.Point5.Phase.Should().Be(Phases.TwoPhase);
+        }
+
+        [Test]
+        public void TestPoint6()
+        {
+            Cycle.Point6.Pressure.Should().Be(Cycle.Condenser.Pressure);
+            Cycle.Point6.Quality.Should().Be(TwoPhase.Bubble.VaporQuality());
+            Cycle.Point6.Phase.Should().Be(Phases.TwoPhase);
+        }
+
+        [Test]
+        public void TestPoint7()
+        {
+            Cycle.Point7.Pressure.Should().Be(Cycle.Condenser.Pressure);
+            Cycle.Point7.Temperature.Should().Be(Cycle.Point6.Temperature - Cycle.Condenser.Subcooling);
+            Cycle.Point7.Phase.Should().Be(Phases.Liquid);
+        }
+
+        [Test]
+        public void TestPoint8()
+        {
+            Cycle.Point8.Pressure.Should().Be(Cycle.Economizer.Pressure);
+            Cycle.Point8.Enthalpy.Should().Be(Cycle.Point7.Enthalpy);
+            Cycle.Point8.Phase.Should().Be(Phases.TwoPhase);
+        }
+
+        [Test]
+        public void TestPoint9()
+        {
+            Cycle.Point9.Pressure.Should().Be(Cycle.Economizer.Pressure);
+            Cycle.Point9.Enthalpy.Should().Be(
+                ((Cycle.Point8.Enthalpy.JoulesPerKilogram * (Cycle.Point2.Enthalpy.JoulesPerKilogram -
+                                                             Cycle.Point3.Enthalpy.JoulesPerKilogram) +
+                  Cycle.Point3.Enthalpy.JoulesPerKilogram * (Cycle.Point7.Enthalpy.JoulesPerKilogram -
+                                                             Cycle.Point10.Enthalpy.JoulesPerKilogram)) /
+                 (Cycle.Point2.Enthalpy.JoulesPerKilogram - Cycle.Point3.Enthalpy.JoulesPerKilogram +
+                     Cycle.Point7.Enthalpy.JoulesPerKilogram - Cycle.Point10.Enthalpy.JoulesPerKilogram))
+                .JoulesPerKilogram());
+            Cycle.Point9.Phase.Should().Be(Phases.TwoPhase);
+        }
+
+        [Test]
+        public void TestPoint10()
+        {
+            Cycle.Point10.Pressure.Should().Be(Cycle.Condenser.Pressure);
+            Cycle.Point10.Temperature.Should().Be(Cycle.Point8.Temperature + Cycle.Economizer.TemperatureDifference);
+            Cycle.Point10.Phase.Should().Be(Phases.Liquid);
+        }
+
+        [Test]
+        public void TestPoint11()
+        {
+            Cycle.Point11.Pressure.Should().Be(Cycle.Evaporator.Pressure);
+            Cycle.Point11.Enthalpy.Should().Be(Cycle.Point10.Enthalpy);
+            Cycle.Point11.Phase.Should().Be(Phases.TwoPhase);
+        }
+    }
+}
---
+++ VCRC/VCRCWithEconomizerTPI.cs
@@ -0,0 +1,147 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using FluentValidation;
+using SharpProp;
+using UnitsNet;
+using UnitsNet.NumberExtensions.NumberToSpecificEnergy;
+using UnitsNet.Units;
+using VCRC.Components;
+using VCRC.Extensions;
+using VCRC.Fluids;
+using VCRC.Validators;
+
+namespace VCRC
+{
+    /// <summary>
+    ///     Two-stage VCRC with economizer and two-phase injection to the compressor.
+    /// </summary>
+    public class VCRCWithEconomizerTPI : TwoStageSubcriticalVCRC
+    {
+        /// <summary>
+        ///     Two-stage VCRC with economizer and two-phase injection to the compressor.
+        /// </summary>
+        /// <param name="evaporator">Evaporator.</param>
+        /// <param name="compressor">Compressor.</param>
+        /// <param name="condenser">Condenser.</param>
+        /// <param name="economizer">Economizer.</param>
+        public VCRCWithEconomizerTPI(Evaporator evaporator, Compressor compressor, Condenser condenser,
+            EconomizerTPI economizer) : base(evaporator, compressor, condenser)
+        {
+            Economizer = economizer;
+            Point2s = Refrigerant.WithState(Input.Pressure(Economizer.Pressure), Input.Entropy(Point1.Entropy));
+            var isentropicSpecificWork1 = Point2s.Enthalpy - Point1.Enthalpy;
+            var specificWork1 = isentropicSpecificWork1 / Compressor.IsentropicEfficiency.DecimalFractions;
+            Point2 = Refrigerant.WithState(Input.Pressure(Economizer.Pressure),
+                Input.Enthalpy(Point1.Enthalpy + specificWork1));
+            Point3 = Refrigerant.WithState(Input.Pressure(Economizer.Pressure),
+                Input.Quality(TwoPhase.Dew.VaporQuality()));
+            Point4s = Refrigerant.WithState(Input.Pressure(Condenser.Pressure), Input.Entropy(Point3.Entropy));
+            Point5 = Refrigerant.WithState(Input.Pressure(Condenser.Pressure),
+                Input.Quality(TwoPhase.Dew.VaporQuality()));
+            Point6 = Refrigerant.WithState(Input.Pressure(Condenser.Pressure),
+                Input.Quality(TwoPhase.Bubble.VaporQuality()));
+            Point7 = Condenser.Subcooling == TemperatureDelta.Zero
+                ? Point6.Clone()
+                : Refrigerant.WithState(Input.Pressure(Condenser.Pressure),
+                    Input.Temperature(Point6.Temperature - Condenser.Subcooling));
+            Point8 = Refrigerant.WithState(Input.Pressure(Economizer.Pressure),
+                Input.Enthalpy(Point7.Enthalpy));
+            Point10 = Refrigerant.WithState(Input.Pressure(Condenser.Pressure),
+                Input.Temperature(Point8.Temperature + Economizer.TemperatureDifference));
+            Point9 = Refrigerant.WithState(Input.Pressure(Economizer.Pressure),
+                Input.Enthalpy(((Point8.Enthalpy.JoulesPerKilogram *
+                                 (Point2.Enthalpy.JoulesPerKilogram - Point3.Enthalpy.JoulesPerKilogram) +
+                                 Point3.Enthalpy.JoulesPerKilogram * (Point7.Enthalpy.JoulesPerKilogram -
+                                                                      Point10.Enthalpy.JoulesPerKilogram)) /
+                                (Point2.Enthalpy.JoulesPerKilogram - Point3.Enthalpy.JoulesPerKilogram +
+                                    Point7.Enthalpy.JoulesPerKilogram - Point10.Enthalpy.JoulesPerKilogram))
+                    .JoulesPerKilogram().ToUnit(SpecificEnergyUnit.KilojoulePerKilogram)));
+            new VCRCWithEconomizerTPIValidator().ValidateAndThrow(this);
+            Point11 = Refrigerant.WithState(Input.Pressure(Evaporator.Pressure), Input.Enthalpy(Point10.Enthalpy));
+            SecondStageSpecificMassFlow = FirstStageSpecificMassFlow *
+                                          (1 + (Point2.Enthalpy - Point3.Enthalpy) /
+                                              (Point3.Enthalpy - Point9.Enthalpy));
+            var isentropicSpecificWork2 =
+                SecondStageSpecificMassFlow.DecimalFractions * (Point4s.Enthalpy - Point3.Enthalpy);
+            var specificWork2 = isentropicSpecificWork2 / Compressor.IsentropicEfficiency.DecimalFractions;
+            Point4 = Refrigerant.WithState(Input.Pressure(Condenser.Pressure),
+                Input.Enthalpy(Point3.Enthalpy + specificWork2 / SecondStageSpecificMassFlow.DecimalFractions));
+            IsentropicSpecificWork = isentropicSpecificWork1 + isentropicSpecificWork2;
+            SpecificWork = specificWork1 + specificWork2;
+            SpecificCoolingCapacity = Point1.Enthalpy - Point11.Enthalpy;
+            SpecificHeatingCapacity =
+                SecondStageSpecificMassFlow.DecimalFractions * (Point4.Enthalpy - Point7.Enthalpy);
+        }
+        
+        /// <summary>
+        ///     Economizer as VCRC component.
+        /// </summary>
+        public EconomizerTPI Economizer { get; }
+        
+        /// <summary>
+        ///     Point 2s – first isentropic compression stage discharge.
+        /// </summary>
+        [SuppressMessage("ReSharper", "InconsistentNaming")]
+        public Refrigerant Point2s { get; }
+
+        /// <summary>
+        ///     Point 2 – first compression stage discharge.
+        /// </summary>
+        public Refrigerant Point2 { get; }
+
+        /// <summary>
+        ///     Point 3 – second compression stage suction.
+        /// </summary>
+        public Refrigerant Point3 { get; }
+
+        /// <summary>
+        ///     Point 4s – second isentropic compression stage discharge.
+        /// </summary>
+        [SuppressMessage("ReSharper", "InconsistentNaming")]
+        public Refrigerant Point4s { get; }
+
+        /// <summary>
+        ///     Point 4 – second compression stage discharge / condenser inlet.
+        /// </summary>
+        public Refrigerant Point4 { get; }
+
+        /// <summary>
+        ///     Point 5 – dew-point on the condensing isobar.
+        /// </summary>
+        public Refrigerant Point5 { get; }
+
+        /// <summary>
+        ///     Point 6 – bubble-point on the condensing isobar.
+        /// </summary>
+        public Refrigerant Point6 { get; }
+
+        /// <summary>
+        ///     Point 7 – condenser outlet /
+        ///     inlet to the expansion valve of the injection circuit /
+        ///     inlet of the main stream into the economizer ("hot" inlet).
+        /// </summary>
+        public Refrigerant Point7 { get; }
+        
+        /// <summary>
+        ///     Point 8 – outlet from the expansion valve of the injection circuit /
+        ///     inlet of the injected stream into the economizer ("cold" inlet).
+        /// </summary>
+        public Refrigerant Point8 { get; }
+        
+        /// <summary>
+        ///     Point 9 – outlet of the injected stream from the economizer ("cold" outlet) /
+        ///     injection of two-phase refrigerant into the compressor.
+        /// </summary>
+        public Refrigerant Point9 { get; }
+
+        /// <summary>
+        ///     Point 10 – outlet of the main stream from the economizer ("hot" outlet) /
+        ///     inlet to the expansion valve of the evaporator circuit.
+        /// </summary>
+        public Refrigerant Point10 { get; }
+
+        /// <summary>
+        ///     Point 11 – outlet from the expansion valve of the evaporator circuit / evaporator inlet.
+        /// </summary>
+        public Refrigerant Point11 { get; }
+    }
+}
