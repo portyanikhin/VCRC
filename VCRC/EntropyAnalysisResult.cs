268a206b5540a6d24e54d811d2d0eef47f30ce7d
Vladimir Portyanikhin <portyanikhin@users.noreply.github.com>
2021-11-24 12:32:50 +0300

    Added the IEntropyAnalysable interface and the EntropyAnalysisResult record

---
+++ VCRC/EntropyAnalysisResult.cs
@@ -0,0 +1,31 @@
+﻿using UnitsNet;
+
+namespace VCRC
+{
+    /// <summary>
+    ///     Result of the VCRC entropy analysis.
+    /// </summary>
+    /// <param name="ThermodynamicPerfection">Degree of thermodynamic perfection of the cycle (by default, %).</param>
+    /// <param name="MinSpecificWorkRatio">
+    ///     Minimum specific work (its percentage of the total specific work) (by default, %).
+    /// </param>
+    /// <param name="CompressorEnergyLossRatio">
+    ///     Energy losses in the compressor (its percentage of the total specific work) (by default, %).
+    /// </param>
+    /// <param name="CondenserEnergyLossRatio">
+    ///     Minimum required specific work to compensate for entropy production in the condenser
+    ///     (its percentage of the total specific work) (by default, %).
+    /// </param>
+    /// <param name="ExpansionValvesEnergyLossRatio">
+    ///     Minimum required specific work to compensate for entropy production in the expansion valves
+    ///     (its percentage of the total specific work) (by default, %).
+    /// </param>
+    /// <param name="EvaporatorEnergyLossRatio">
+    ///     Minimum required specific work to compensate for entropy production in the evaporator
+    ///     (its percentage of the total specific work) (by default, %).
+    /// </param>
+    /// <param name="AnalysisRelativeError">Entropy analysis relative error (by default, %).</param>
+    public record EntropyAnalysisResult(Ratio ThermodynamicPerfection, Ratio MinSpecificWorkRatio,
+        Ratio CompressorEnergyLossRatio, Ratio CondenserEnergyLossRatio, Ratio ExpansionValvesEnergyLossRatio, 
+        Ratio EvaporatorEnergyLossRatio, Ratio AnalysisRelativeError);
+}
