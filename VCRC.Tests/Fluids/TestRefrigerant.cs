d14733ac46325c485afaeb2c04fcb788645ee964
Vladimir Portyanikhin <portyanikhin@users.noreply.github.com>
2022-03-27 19:48:28 +0300

    Update unit tests

--- VCRC.Tests/Fluids/TestRefrigerant.cs
+++ VCRC.Tests/Fluids/TestRefrigerant.cs
@@ -20,14 +20,27 @@ public static class TestRefrigerant
     }
 
     [Test(ExpectedResult = 22.064e6)]
-    public static double TestCriticalPressure() => Refrigerant.CriticalPressure.Pascals;
+    public static double TestCriticalPressure() =>
+        Refrigerant.CriticalPressure.Pascals;
 
     [Test(ExpectedResult = 373.946)]
-    public static double TestCriticalTemperature() => Refrigerant.CriticalTemperature.DegreesCelsius;
+    public static double TestCriticalTemperature() =>
+        Refrigerant.CriticalTemperature.DegreesCelsius;
 
     [Test(ExpectedResult = 611.65480089686844)]
-    public static double TestTriplePressure() => Refrigerant.TriplePressure.Pascals;
+    public static double TestTriplePressure() =>
+        Refrigerant.TriplePressure.Pascals;
 
     [Test(ExpectedResult = 0.010000000000047748)]
-    public static double TestTripleTemperature() => Refrigerant.TripleTemperature.DegreesCelsius;
+    public static double TestTripleTemperature() =>
+        Refrigerant.TripleTemperature.DegreesCelsius;
+
+    [TestCase(FluidsList.R22, ExpectedResult = false)]
+    [TestCase(FluidsList.R32, ExpectedResult = false)]
+    [TestCase(FluidsList.R134a, ExpectedResult = false)]
+    [TestCase(FluidsList.R404A, ExpectedResult = true)]
+    [TestCase(FluidsList.R407C, ExpectedResult = true)]
+    [TestCase(FluidsList.R410A, ExpectedResult = true)]
+    public static bool TestHasGlide(FluidsList name) =>
+        new Refrigerant(name).HasGlide;
 }
--- VCRC.Tests/Fluids/TestTwoPhase.cs
+++ VCRC.Tests/Fluids/TestTwoPhase.cs
@@ -8,5 +8,6 @@ public static class TestTwoPhase
 {
     [TestCase(TwoPhase.Bubble, ExpectedResult = 0)]
     [TestCase(TwoPhase.Dew, ExpectedResult = 100)]
-    public static double TestVaporQuality(TwoPhase member) => member.VaporQuality().Percent;
+    public static double TestVaporQuality(TwoPhase member) =>
+        member.VaporQuality().Percent;
 }
