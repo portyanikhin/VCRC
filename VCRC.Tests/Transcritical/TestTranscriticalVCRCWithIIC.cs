ba4661c6dfd9f641c046905d2215167b02531638
Vladimir Portyanikhin <portyanikhin@users.noreply.github.com>
2022-05-14 20:45:51 +0300

    Added the TranscriticalVCRCWithIIC class

---
+++ VCRC.Tests/Transcritical/TestTranscriticalVCRCWithIIC.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using FluentAssertions;
+using FluentValidation;
+using NUnit.Framework;
+using SharpProp;
+using UnitsNet;
+using UnitsNet.NumberExtensions.NumberToPressure;
+using UnitsNet.NumberExtensions.NumberToRatio;
+using UnitsNet.NumberExtensions.NumberToTemperature;
+using VCRC.Components;
+using VCRC.Extensions;
+using VCRC.Fluids;
+using VCRC.Transcritical;
+
+namespace VCRC.Tests.Transcritical;
+
+public class TestTranscriticalVCRCWithIIC
+{
+    private TranscriticalVCRCWithIIC Cycle { get; set; } = null!;
+
+    [OneTimeSetUp]
+    public void SetUp()
+    {
+        const FluidsList refrigerantName = FluidsList.R744;
+        var evaporator = new Evaporator(refrigerantName,
+            5.DegreesCelsius(), TemperatureDelta.FromKelvins(8));
+        var compressor = new Compressor(80.Percent());
+        var gasCooler = new GasCooler(refrigerantName, 40.DegreesCelsius());
+        Cycle = new TranscriticalVCRCWithIIC(evaporator, compressor, gasCooler);
+    }
+
+    [Test]
+    public void TestWrongRefrigerant()
+    {
+        const FluidsList refrigerantName = FluidsList.R407C;
+        var evaporator = new Evaporator(refrigerantName,
+            Cycle.Evaporator.Temperature, Cycle.Evaporator.Superheat);
+        var gasCooler = new GasCooler(refrigerantName, 90.DegreesCelsius(),
+            new Refrigerant(refrigerantName).CriticalPressure + 1.Bars());
+        Action action = () =>
+            _ = new TranscriticalVCRCWithIIC(evaporator, Cycle.Compressor, gasCooler);
+        action.Should().Throw<ValidationException>()
+            .WithMessage("*Refrigerant should not have a temperature glide!*");
+    }
+
+    [TestCase(Bound.Lower, "Intermediate pressure should be greater than evaporating pressure!")]
+    [TestCase(Bound.Higher, "Intermediate pressure should be less than gas cooler pressure!")]
+    public void TestWrongIntermediatePressure(Bound bound, string message)
+    {
+        Action action = () =>
+            _ = new TranscriticalVCRCWithIIC(
+                Cycle.Evaporator, Cycle.Compressor, Cycle.GasCooler,
+                new IntermediateVessel(bound is Bound.Lower
+                    ? Cycle.Evaporator.Pressure
+                    : Cycle.GasCooler.Pressure));
+        action.Should().Throw<ValidationException>().WithMessage($"*{message}*");
+    }
+
+    [Test]
+    public void TestWrongPhaseAtIntermediateVesselInlet()
+    {
+        Action action = () =>
+            _ = new TranscriticalVCRCWithIIC(
+                Cycle.Evaporator, Cycle.Compressor, Cycle.GasCooler,
+                new IntermediateVessel(Cycle.GasCooler.Pressure - 1.Pascals()));
+        action.Should().Throw<ValidationException>()
+            .WithMessage("*There should be a two-phase refrigerant at the intermediate vessel inlet!*");
+    }
+
+    [Test]
+    public void TestSpecificMassFlows()
+    {
+        Cycle.FirstStageSpecificMassFlow.Should().Be(100.Percent());
+        Cycle.SecondStageSpecificMassFlow.Should().Be(
+            Cycle.FirstStageSpecificMassFlow / (1 - Cycle.Point6.Quality!.Value.DecimalFractions));
+    }
+
+    [Test]
+    [SuppressMessage("ReSharper", "InconsistentNaming")]
+    public void TestPoint2s()
+    {
+        Cycle.Point2s.Pressure.Should().Be(Cycle.IntermediateVessel.Pressure);
+        Cycle.Point2s.Entropy.Should().Be(Cycle.Point1.Entropy);
+        Cycle.Point2s.Phase.Should().Be(Phases.SupercriticalGas);
+    }
+
+    [Test]
+    public void TestPoint2()
+    {
+        Cycle.Point2.Pressure.Should().Be(Cycle.IntermediateVessel.Pressure);
+        Cycle.Point2.Enthalpy.Should().Be(
+            Cycle.Point1.Enthalpy + (Cycle.Point2s.Enthalpy - Cycle.Point1.Enthalpy) /
+            Cycle.Compressor.IsentropicEfficiency.DecimalFractions);
+        Cycle.Point2.Phase.Should().Be(Phases.SupercriticalGas);
+    }
+
+    [Test]
+    public void TestPoint3()
+    {
+        Cycle.Point3.Pressure.Should().Be(Cycle.IntermediateVessel.Pressure);
+        Cycle.Point3.Enthalpy.Should().Be(
+            (Cycle.FirstStageSpecificMassFlow.DecimalFractions * Cycle.Point2.Enthalpy +
+             (Cycle.SecondStageSpecificMassFlow - Cycle.FirstStageSpecificMassFlow).DecimalFractions *
+             Cycle.Point7.Enthalpy) / Cycle.SecondStageSpecificMassFlow.DecimalFractions);
+        Cycle.Point3.Phase.Should().Be(Phases.SupercriticalGas);
+    }
+
+    [Test]
+    [SuppressMessage("ReSharper", "InconsistentNaming")]
+    public void TestPoint4s()
+    {
+        Cycle.Point4s.Pressure.Should().Be(Cycle.GasCooler.Pressure);
+        Cycle.Point4s.Entropy.Should().Be(Cycle.Point3.Entropy);
+        Cycle.Point4s.Phase.Should().Be(Phases.Supercritical);
+    }
+
+    [Test]
+    public void TestPoint4()
+    {
+        Cycle.Point4.Pressure.Should().Be(Cycle.GasCooler.Pressure);
+        Cycle.Point4.Enthalpy.Should().Be(
+            Cycle.Point3.Enthalpy + (Cycle.Point4s.Enthalpy - Cycle.Point3.Enthalpy) /
+            Cycle.Compressor.IsentropicEfficiency.DecimalFractions);
+        Cycle.Point4.Enthalpy.Should().BeGreaterThan(Cycle.Point4s.Enthalpy);
+        Cycle.Point4.Phase.Should().Be(Phases.Supercritical);
+    }
+
+    [Test]
+    public void TestPoint5()
+    {
+        Cycle.Point5.Pressure.Should().Be(Cycle.GasCooler.Pressure);
+        Cycle.Point5.Temperature.Should().Be(Cycle.GasCooler.Temperature);
+        Cycle.Point5.Phase.Should().Be(Phases.Supercritical);
+    }
+
+    [Test]
+    public void TestPoint6()
+    {
+        Cycle.Point6.Pressure.Should().Be(Cycle.IntermediateVessel.Pressure);
+        Cycle.Point6.Enthalpy.Should().Be(Cycle.Point5.Enthalpy);
+        Cycle.Point6.Phase.Should().Be(Phases.TwoPhase);
+    }
+
+    [Test]
+    public void TestPoint7()
+    {
+        Cycle.Point7.Pressure.Should().Be(Cycle.IntermediateVessel.Pressure);
+        Cycle.Point7.Quality.Should().Be(TwoPhase.Dew.VaporQuality());
+        Cycle.Point7.Phase.Should().Be(Phases.TwoPhase);
+    }
+
+    [Test]
+    public void TestPoint8()
+    {
+        Cycle.Point8.Pressure.Should().Be(Cycle.IntermediateVessel.Pressure);
+        Cycle.Point8.Quality.Should().Be(TwoPhase.Bubble.VaporQuality());
+        Cycle.Point8.Phase.Should().Be(Phases.TwoPhase);
+    }
+
+    [Test]
+    public void TestPoint9()
+    {
+        Cycle.Point9.Pressure.Should().Be(Cycle.Evaporator.Pressure);
+        Cycle.Point9.Enthalpy.Should().Be(Cycle.Point8.Enthalpy);
+        Cycle.Point9.Phase.Should().Be(Phases.TwoPhase);
+    }
+
+    [Test]
+    public void TestEntropyAnalysis()
+    {
+        var result =
+            Cycle.EntropyAnalysis(18.DegreesCelsius(), 35.DegreesCelsius());
+        const double tolerance = 1e-10;
+        result.ThermodynamicPerfection.Percent
+            .Should().BeApproximately(17.14102907821518, tolerance);
+        result.MinSpecificWorkRatio.Percent
+            .Should().BeApproximately(17.063415588517042, tolerance);
+        result.CompressorEnergyLossRatio.Percent
+            .Should().BeApproximately(20, tolerance);
+        result.CondenserEnergyLossRatio.Percent
+            .Should().Be(0);
+        result.GasCoolerEnergyLossRatio.Percent
+            .Should().BeApproximately(21.022190486946972, tolerance);
+        result.ExpansionValvesEnergyLossRatio.Percent
+            .Should().BeApproximately(26.061305006201664, tolerance);
+        result.EvaporatorEnergyLossRatio.Percent
+            .Should().BeApproximately(14.071452712259557, tolerance);
+        result.RecuperatorEnergyLossRatio.Percent
+            .Should().Be(0);
+        result.EconomizerEnergyLossRatio.Percent
+            .Should().Be(0);
+        result.MixingEnergyLossRatio.Percent
+            .Should().BeApproximately(1.7816362060747746, tolerance);
+        result.AnalysisRelativeError.Percent
+            .Should().BeApproximately(0.4548531874847325, tolerance);
+        result.Sum().Percent
+            .Should().BeApproximately(100, tolerance);
+    }
+}
---
+++ VCRC/Transcritical/TranscriticalVCRCWithIIC.cs
@@ -0,0 +1,57 @@
+﻿using FluentValidation;
+using VCRC.Abstract;
+using VCRC.Components;
+using VCRC.Fluids;
+
+namespace VCRC.Transcritical;
+
+/// <summary>
+///     Two-stage transcritical VCRC with incomplete intercooling.
+/// </summary>
+public class TranscriticalVCRCWithIIC : AbstractVCRCWithIIC
+{
+    /// <summary>
+    ///     Two-stage transcritical VCRC with incomplete intercooling.
+    /// </summary>
+    /// <remarks>
+    ///     If an intermediate vessel is not specified, it will be constructed automatically.
+    /// </remarks>
+    /// <param name="evaporator">Evaporator.</param>
+    /// <param name="compressor">Compressor.</param>
+    /// <param name="gasCooler">Gas cooler.</param>
+    /// <param name="intermediateVessel">Intermediate vessel (optional).</param>
+    /// <exception cref="ValidationException">
+    ///     Only one refrigerant should be selected!
+    /// </exception>
+    /// <exception cref="ValidationException">
+    ///     Refrigerant should not have a temperature glide!
+    /// </exception>
+    /// <exception cref="ValidationException">
+    ///     Intermediate pressure should be greater than evaporating pressure!
+    /// </exception>
+    /// <exception cref="ValidationException">
+    ///     Intermediate pressure should be less than gas cooler pressure!
+    /// </exception>
+    /// <exception cref="ValidationException">
+    ///     There should be a two-phase refrigerant at the intermediate vessel inlet!
+    /// </exception>
+    public TranscriticalVCRCWithIIC(Evaporator evaporator, Compressor compressor, GasCooler gasCooler,
+        IntermediateVessel? intermediateVessel = null) :
+        base(evaporator, compressor, gasCooler, intermediateVessel) =>
+        GasCooler = gasCooler;
+
+    /// <summary>
+    ///     Gas cooler as a transcritical VCRC component.
+    /// </summary>
+    public GasCooler GasCooler { get; }
+
+    /// <summary>
+    ///     Point 4 – second compression stage discharge / gas cooler inlet.
+    /// </summary>
+    public new Refrigerant Point4 => base.Point4;
+
+    /// <summary>
+    ///     Point 5 – gas cooler outlet / first EV inlet.
+    /// </summary>
+    public new Refrigerant Point5 => base.Point5;
+}
