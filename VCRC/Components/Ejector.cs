b19f66541541a76bcca71424af5ce1476b347a6d
Vladimir Portyanikhin <portyanikhin@users.noreply.github.com>
2022-05-23 22:04:56 +0300

    Added the Ejector record

---
+++ VCRC/Components/Ejector.cs
@@ -0,0 +1,68 @@
+﻿using FluentValidation;
+using UnitsNet;
+
+namespace VCRC;
+
+/// <summary>
+///     Ejector as a VCRC component.
+/// </summary>
+public record Ejector
+{
+    /// <summary>
+    ///     Ejector as a VCRC component.
+    /// </summary>
+    /// <param name="isentropicEfficiency">
+    ///     Isentropic efficiency of the nozzle, suction section and diffuser.
+    /// </param>
+    /// <exception cref="ValidationException">
+    ///     Isentropic efficiency of the nozzle should be in (0;100) %!
+    /// </exception>
+    /// <exception cref="ValidationException">
+    ///     Isentropic efficiency of the suction section should be in (0;100) %!
+    /// </exception>
+    /// <exception cref="ValidationException">
+    ///     Isentropic efficiency of the diffuser should be in (0;100) %!
+    /// </exception>
+    public Ejector(Ratio isentropicEfficiency) :
+        this(isentropicEfficiency, isentropicEfficiency, isentropicEfficiency)
+    {
+    }
+
+    /// <summary>
+    ///     Ejector as a VCRC component.
+    /// </summary>
+    /// <param name="nozzleIsentropicEfficiency">Isentropic efficiency of the nozzle.</param>
+    /// <param name="suctionIsentropicEfficiency">Isentropic efficiency of the suction section.</param>
+    /// <param name="diffuserIsentropicEfficiency">Isentropic efficiency of the diffuser.</param>
+    /// <exception cref="ValidationException">
+    ///     Isentropic efficiency of the nozzle should be in (0;100) %!
+    /// </exception>
+    /// <exception cref="ValidationException">
+    ///     Isentropic efficiency of the suction section should be in (0;100) %!
+    /// </exception>
+    /// <exception cref="ValidationException">
+    ///     Isentropic efficiency of the diffuser should be in (0;100) %!
+    /// </exception>
+    public Ejector(Ratio nozzleIsentropicEfficiency, Ratio suctionIsentropicEfficiency,
+        Ratio diffuserIsentropicEfficiency)
+    {
+        (NozzleIsentropicEfficiency, SuctionIsentropicEfficiency, DiffuserIsentropicEfficiency) =
+            (nozzleIsentropicEfficiency, suctionIsentropicEfficiency, diffuserIsentropicEfficiency);
+        new EjectorValidator().ValidateAndThrow(this);
+    }
+
+    /// <summary>
+    ///     Isentropic efficiency of the nozzle.
+    /// </summary>
+    public Ratio NozzleIsentropicEfficiency { get; }
+
+    /// <summary>
+    ///     Isentropic efficiency of the suction section.
+    /// </summary>
+    public Ratio SuctionIsentropicEfficiency { get; }
+
+    /// <summary>
+    ///     Isentropic efficiency of the diffuser.
+    /// </summary>
+    public Ratio DiffuserIsentropicEfficiency { get; }
+}
---
+++ VCRC/Components/Validators/EjectorValidator.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using UnitsNet;
+using UnitsNet.NumberExtensions.NumberToRatio;
+
+namespace VCRC;
+
+internal class EjectorValidator : AbstractValidator<Ejector>
+{
+    internal EjectorValidator()
+    {
+        RuleFor(ejector => ejector.NozzleIsentropicEfficiency)
+            .ExclusiveBetween(Ratio.Zero, 100.Percent())
+            .WithMessage("Isentropic efficiency of the nozzle should be in (0;100) %!");
+        RuleFor(ejector => ejector.SuctionIsentropicEfficiency)
+            .ExclusiveBetween(Ratio.Zero, 100.Percent())
+            .WithMessage("Isentropic efficiency of the suction section should be in (0;100) %!");
+        RuleFor(ejector => ejector.DiffuserIsentropicEfficiency)
+            .ExclusiveBetween(Ratio.Zero, 100.Percent())
+            .WithMessage("Isentropic efficiency of the diffuser should be in (0;100) %!");
+    }
+}
