9c4e3cb39efeba84d12ed548224ea78c583f80f0
Vladimir Portyanikhin <portyanikhin@users.noreply.github.com>
2022-06-06 12:52:19 +0300

    Added the VCRCWithEjectorEconomizerAndTPI class

---
+++ VCRC/Validators/VCRCWithEjectorEconomizerAndTPIValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace VCRC;
+
+internal class VCRCWithEjectorEconomizerAndTPIValidator : AbstractValidator<VCRCWithEjectorEconomizerAndTPI>
+{
+    internal VCRCWithEjectorEconomizerAndTPIValidator()
+    {
+        RuleFor(vcrc => vcrc.Point6.Temperature + vcrc.Economizer.TemperatureDifference)
+            .LessThan(vcrc => vcrc.Point5.Temperature)
+            .WithMessage("Too high temperature difference at economizer 'cold' side!");
+    }
+}
